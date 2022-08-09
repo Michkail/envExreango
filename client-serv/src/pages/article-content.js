c475b15cd4e430a040709c8a2e407bfdf99438e7
unknown <pitermichkail@ymail.com>
2022-08-09 17:06:18 +0700

    ..

---
+++ client-serv/src/pages/article-content.js
@@ -0,0 +1,24 @@
+const articles = [
+  {
+    name: 'hmm',
+    title: 'hmmm',
+    thumbnail: 'https://i.picsum.photos/id/803/200/300.jpg?hmac=v-3AsAcUOG4kCDsLMlWF9_3Xa2DTODGyKLggZNvReno',
+    content: [
+      `lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+      ``,
+      ``
+    ],
+  },
+  {
+    name: 'huft',
+    title: 'hufty',
+    thumbnail: 'https://i.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk',
+    content: [
+      ``,
+      ``,
+      ``
+    ]
+  }
+];
+
+export default articles;
